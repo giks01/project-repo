8daf8b0a4a01a103c6b6996dc63bf40f1305956a
Michael Munene <151326716+giks01@users.noreply.github.com>
2025-12-04 06:21:10 +0300

    Update guiexercise.py with latest version from main branch
    Added exit button and improved error handling.

--- guiexercise.py
+++ guiexercise.py
@@ -1,5 +1,8 @@
 import FreeSimpleGUI as sg
+from FreeSimpleGUI import theme
 from feet_meters import get_meters
+
+sg.theme("Black")
 
 feet_label = sg.Text("Enter Feet")
 feet_input = sg.Input(key="feet")
@@ -9,20 +12,27 @@ inches_input = sg.Input(key="inches")
 
 button = sg.Button("Convert")
 output1 = sg.Text(key="output")
+exit_button = sg.Button("Exit")
 
 window = sg.Window("Convertor",
-                   layout=[[feet_label, feet_input],
-                           [inches_label, inches_input],
-                           [button, output1]])
+                    layout=[[feet_label, feet_input],
+                            [inches_label, inches_input],
+                            [button, exit_button, output1]])
+
 while True:
     events, values = window.read()
-    feet = float(values["feet"])
-    inches = float(values["inches"])
-
-    result =get_meters(feet, inches)
-    window["output"].update(value=f"{result}m", text_color = 'blue')
     if events == sg.WIN_CLOSED:
         break
+    elif events == "Exit":
+        break
 
+    try:
+        feet = float(values["feet"])
+        inches = float(values["inches"])
+
+        result =get_meters(feet, inches)
+        window["output"].update(value=f"{result}m", text_color = 'white')
+    except ValueError:
+        sg.popup("Please provide input", font=('Helvetica', 10))
 
 window.close()
